84a8e65c832cfc726faf5e1e4c90de5418bfda9d
PCBZ <devil02047@outlook.com>
2024-12-23 11:34:16 -0800

    add

--- test.py
+++ test.py
@@ -1,14 +1,39 @@
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        map1 = {}
-        map2 = {}
-        for i, ch in enumerate(s):
-            if  ch in map1 and map1[ch] != t[i] or ( t[i] in map2 and map2[t[i]] != ch ):
-                return False
-            map1[ch] = t[i]
-            map2[t[i]] = ch
-        return True
+from collections import deque
 
-s = 'badc'
-t = 'baba'
-print(Solution().isIsomorphic(s, t))
+class MyStack:
+
+    def __init__(self):
+        self.queue1 = deque([])
+        self.queue2 = deque([])
+
+    def push(self, x: int) -> None:
+        queue = self.queue1 if len(self.queue1) > 0 else self.queue2
+        queue.append(x)
+
+    def pop(self) -> int:
+        queue = self.queue1 if len(self.queue1) > 0 else self.queue2
+        queue_backup = self.queue1 if len(self.queue2) > 0 else self.queue2
+        while len(queue) > 1:
+            queue_backup.append(queue.popleft())
+        return queue.popleft()
+
+    def top(self) -> int:
+        queue = self.queue1 if len(self.queue1) > 0 else self.queue2
+        queue_backup = self.queue1 if len(self.queue2) > 0 else self.queue2
+        while len(queue) > 1:
+            queue_backup.append(queue.popleft())
+        val = queue[0]
+        queue_backup.append(queue.popleft())
+        return val
+
+    def empty(self) -> bool:
+        queue = self.queue1 if len(self.queue1) > 0 else self.queue2
+        return queue
+
+
+# Your MyStack object will be instantiated and called as such:
+obj = MyStack()
+obj.push(1)
+print(obj.top())
+print(obj.pop())
+print(obj.empty())
