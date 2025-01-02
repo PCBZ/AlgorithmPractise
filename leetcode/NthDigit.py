0170bb01403cb00cc20ecdb1166c89845abba7d7
PCBZ <devil02047@outlook.com>
2025-01-02 14:08:13 -0800

    add

---
+++ leetcode/NthDigit.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findNthDigit(self, n: int) -> int:
+        digit_count = 1
+        range_count = 9
+        prev_num = 0
+        while n > prev_num + digit_count * range_count:
+            prev_num += digit_count * range_count
+            digit_count += 1
+            range_count *= 10
+        
+        print(digit_count, range_count, prev_num)
+        idx = 10 ** (digit_count - 1) + ( n - prev_num - 1 ) // digit_count
+        offset = (n - prev_num - 1) % digit_count
+        print(idx, offset)
+        return int(str(idx)[offset])
+
+
+if __name__ == "__main__":
+    print(Solution().findNthDigit(1000))
