111881bd3d8ab8396267ce62dcd82d02cb99bbc5
PCBZ <devil02047@outlook.com>
2025-01-31 13:55:50 -0800

    add

--- test.py
+++ test.py
@@ -13,15 +13,29 @@ class Solution:
 
 print(Solution().isPerfectSquare(16))
 
+def consective_ones(num: int) -> int:
+    prev_digit = 0
+    while num > 0:
+        cur_digit = num & 1
+        if prev_digit == 1 and cur_digit == 1:
+            print(num)
+            return 0
+        num >>= 1
+        prev_digit = cur_digit
+    return 1
 
-print((ord('z') - ord('a')) % 26)
+consective_ones(3)
 
-summ = 0
-for i in range(1, 11):
-    summ += i * (2 ** (i - 1))
-print(summ)
+class Solution:
+    def findIntegers(self, n: int) -> int:
+        def consective_ones(num: int) -> int:
+            prev_digit = 0
+            while num > 0:
+                cur_digit = num & 1
+                if prev_digit == 1 and cur_digit == 1:
+                    return 0
+                num >>= 1
+                prev_digit = cur_digit
+            return 1
 
-print(math.log2(1))
-
-a = [1, 2, 3]
-print(a[1:], a[:-1])
+        return reduce(lambda accum, i: accum + consective_ones(i), range(n+1), 0)
