c31b54ef7d74d424719ff91015659b1092f7db9d
PCBZ <devil02047@outlook.com>
2025-02-17 11:20:13 -0800

    add

---
+++ leetcode/Interleaving_String.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        m, n = len(s1), len(s2)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            for j in range(n + 1):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[j-1]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i-1]
+                else:
+                    dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+        return dp[-1][-1]
+
+
+
+if __name__ == "__main__":
+    s1 = "aabcc"
+    s2 = "dbbca"
+    s3 = "aadbbbaccc"
+    print(Solution().isInterleave(s1, s2, s3))
